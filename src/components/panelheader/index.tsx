38aef57b7b782f90b8555abb6d591d2220d21020
DesktopMateus <mateusesteves33@gmail.com>
2025-10-25 15:29:31 -0300

    update PanelHeader component styles for improved

--- src/components/panelheader/index.tsx
+++ src/components/panelheader/index.tsx
@@ -8,8 +8,8 @@ export function PanelHeader(){
        await signOut(auth);
     }
     return (
-        <header className="w-full bg-red-500 flex items-center justify-center gap-5 px-4 text-white font-medium 
-        h-10 rounded-lg mb-4">
+        <header className="w-full bg-red-500 flex items-center justify-center gap-5 px-4 text-white font-medium  
+        h-10 rounded-lg mb-4 text-xs md:text-lg">
             <Link to="/dashboard">Dashboard</Link>
             <Link to="/dashboard/new">Cadastrar carro</Link>
             <button onClick={handleSignOut} className="ml-auto">Sair da conta</button>
