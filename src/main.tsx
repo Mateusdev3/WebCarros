3dbd7e3cf435498bd395f471ca544d4351ba1aba
DesktopMateus <mateusesteves33@gmail.com>
2025-11-02 18:20:07 -0300

    Refactor import statement for Toaster from react-hot-toast

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { RouterProvider } from 'react-router-dom'
 import {router} from './routes.tsx'
 import AuthProvider from './contexts/authContext.tsx'
 import {register} from 'swiper/element/bundle'
-import toast, {Toaster} from 'react-hot-toast'
+import {Toaster} from 'react-hot-toast'
 
 register();
 import 'swiper/css';
